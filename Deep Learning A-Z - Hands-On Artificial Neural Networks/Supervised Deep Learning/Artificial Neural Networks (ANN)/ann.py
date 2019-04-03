b2251823f8f823fffd678a6b5cb92ccf022db1e0
jacobskr <31374947+jacobskr@users.noreply.github.com>
2019-04-02 22:44:09 -0400

    ANN - Customer Churn

--- Deep Learning A-Z - Hands-On Artificial Neural Networks/Supervised Deep Learning/Artificial Neural Networks (ANN)/ann.py
+++ Deep Learning A-Z - Hands-On Artificial Neural Networks/Supervised Deep Learning/Artificial Neural Networks (ANN)/ann.py
@@ -12,11 +12,27 @@ dataset = pd.read_csv('Data\\Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
+# Encode categorical independent variables
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+    # Country
+le_X_1 = LabelEncoder()
+X[:, 1] = le_X_1.fit_transform(X[:, 1])
+
+    # Gender
+le_X_2 = LabelEncoder()
+X[:, 2] = le_X_1.fit_transform(X[:, 2])
+
+# Non-ordinal categorical variables (country), so need to create dummy variables
+ohe = OneHotEncoder(categorical_features=[1])
+X = ohe.fit_transform(X).toarray()
+X = X[:, 1:] # Remove first dummy variable
+
 # Splitting the dataset into the Training and Test Set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    test_size = 0.25,
-                                                    random_state=42)
+                                                    test_size = 0.20,
+                                                    random_state=0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
