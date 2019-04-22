0f7732448b929d57a4b4a00a62f16cdeecfccf99
Kyle Jacobs <31374947+jacobskr@users.noreply.github.com>
2019-04-22 13:04:36 -0400

    Python K-Means Clustering - Mall Customers

---
+++ Machine Learning A-Z - Hands-On Python & R Data Science/Clustering/K-Means/kmeans.py
@@ -0,0 +1,54 @@
+# K-Means Clustering
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data\\Mall_Customers.csv')
+X = dataset.iloc[:, [3,4]].values
+
+# Using the elbow method to find optimal number of clusters
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1,11), wcss);
+plt.scatter(x=range(1,11), y=wcss);
+plt.title('The Elbow Method');
+plt.xlabel('Number of Clusters');
+plt.ylabel('WCSS');
+plt.show();
+
+# Applying k-means to the dataset using number of clusters found above
+kmeans = KMeans(n_clusters=5, init='k-means++', max_iter=300, n_init=10)
+y_kmeans = kmeans.fit_predict(X)
+
+# Visualizing the clusters
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1');
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2');
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s=100, c='green', label='Cluster 3');
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s=100, c='cyan', label='Cluster 4');
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s=100, c='magenta', label='Cluster 5');
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label='Centroids');
+plt.title('Clusters of Clients');
+plt.xlabel('Annual Income (k$)');
+plt.ylabel('Spending Score (1-100)');
+plt.legend();
+plt.show();
+
+# After viewing plot, can change label names
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s=100, c='red', label='Sensible');
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s=100, c='blue', label='Target');
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s=100, c='green', label='Standard');
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s=100, c='cyan', label='Careful');
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s=100, c='magenta', label='Careless');
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label='Centroids');
+plt.title('Clusters of Clients');
+plt.xlabel('Annual Income (k$)');
+plt.ylabel('Spending Score (1-100)');
+plt.legend();
+plt.show();
