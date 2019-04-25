6a5eda8c10f220d81a78dd91a6286fa289218916
jacobskr <31374947+jacobskr@users.noreply.github.com>
2019-04-24 23:20:08 -0400

    Restricted Boltzmann Machine - Movie Recommendation System

--- Deep Learning A-Z - Hands-On Artificial Neural Networks/Unsupervised Deep Learning/Boltzmann Machines (BM)/boltzmann_machine.py
+++ Deep Learning A-Z - Hands-On Artificial Neural Networks/Unsupervised Deep Learning/Boltzmann Machines (BM)/boltzmann_machine.py
@@ -31,6 +31,7 @@ test_set = np.array(test_set, dtype='int')
 nb_users = np.unique(np.append(training_set[:,0],test_set[:,0])).size
 nb_movies = np.unique(np.append(training_set[:,1],test_set[:,1])).size
 
+
 # Converting the data into an array with users in lines and movies in columns
 def convert(data):
     new_data = []
@@ -38,4 +39,45 @@ def convert(data):
         id_movies = data[:, 1][data[:, 0] == id_users]
         id_ratings = data[:, 2][data[:, 0] == id_users]
         ratings = np.zeros(nb_movies)
-        ratings[id_movies - 1]
+        ratings[id_movies - 1] = id_ratings
+        new_data.append(list(ratings))
+    return new_data
+
+
+training_set = convert(training_set)
+test_set = convert(test_set)
+
+# Converting the data into Torch tensors
+training_set = torch.FloatTensor(training_set)
+test_set = torch.FloatTensor(test_set)
+
+# Converting the ratings into binary ratings 1 (Liked) or 0 (Not Liked)
+training_set[training_set == 0] = -1
+training_set[training_set == 1] = 0 # or operator doesn't work with pytorch
+training_set[training_set == 2] = 0
+training_set[training_set >= 3] = 1
+test_set[test_set == 0] = -1
+test_set[test_set == 1] = 0
+test_set[test_set == 2] = 0
+test_set[test_set >= 3] = 1
+
+
+# Creating the architecture of the Neural Network
+class RBM():
+    def __init__(self, nv, nh):
+        # Weights - number of hidden and visible nodes
+        self.W = torch.randn(nh, nv) 
+        # Biases
+        self.a = torch.randn(1, nh) # 2d tensor coresponding to batch, bias
+        self.b = torch.randn(1, nv) # 2d tensor coresponding to batch, bias
+        
+
+
+
+
+
+
+
+
+
+
