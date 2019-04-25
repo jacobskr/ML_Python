d174d0c844d813060c6aacbdd678b5541ff8c521
Kyle Jacobs <31374947+jacobskr@users.noreply.github.com>
2019-04-25 16:15:14 -0400

    Simple SVM with Scaling - Medical Predictions

---
+++ Machine Learning and AI - Support Vector Machines in Python/svm_medical.py
@@ -0,0 +1,26 @@
+
+from __future__ import print_function, division
+from future.utils import iteritems
+from builtins import range, input
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler
+
+# load the data
+data = load_breast_cancer()
+
+# split the data into train and test sets
+# this lets us simulate how our model will perform in the future
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(data.data, data.target, test_size=0.33)
+
+# scale the data
+scaler = StandardScaler()
+Xtrain = scaler.fit_transform(Xtrain)
+Xtest = scaler.transform(Xtest)
+
+model = SVC(kernel='rbf')
+# model = SVC()
+model.fit(Xtrain, Ytrain)
+print("train score:", model.score(Xtrain, Ytrain))
+print("test score:", model.score(Xtest, Ytest))
